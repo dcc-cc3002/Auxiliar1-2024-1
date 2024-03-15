9c57bdcda0e711b311cb9c38e4829de3ba73d8e2
Lucas Zulueta <lucas.zulueta.w@gmail.com>
2024-03-15 17:39:29 -0300

    Prueba push 1

---
+++ src/main/scala/example_aux1/HelloWorld.scala
@@ -0,0 +1,15 @@
+package example_aux1
+
+class HelloWorld {
+
+  def hello(name: String): Unit = {
+    println(s"Hello ${name}")
+  }
+
+  def bye(name:String):Unit = {
+    println(s"bye ${name}")
+  }
+  def hi(name:String):Unit = {
+    println(s"hi ${name}")
+  }
+}
