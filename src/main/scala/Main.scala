52b6cfd3b51e65cce19ee99b77719d653b18d318
Nasha <ignacia.galaz@ug.uchile.cl>
2024-08-15 19:49:21 -0400

    chao

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,9 @@
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
+    chao() //tengo que llamar la función
   }
+  def chao(): Unit = {
+    println("Chao Mundo :(")
+ }
 }
