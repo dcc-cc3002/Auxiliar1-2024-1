91af91a0a4cde8411f0279a02c4fdf5242b54812
AguVerdugo <agustin.verdugo@ug.uchile.cl>
2024-08-16 12:07:10 -0400

    Holi

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,10 @@
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
+    function()
+  }
+  def function(): Unit = {
+    println("holi jujuja")
+    println("a")
   }
 }
